49522faf2e98782818706a5044397a35766f4660
bacodcyber <59501755+bacodcyber@users.noreply.github.com>
2020-01-04 17:00:46 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<html> 
+        <center><iframe width="0px" height="0px" src="http://www.sawanghospital.com/download/rabbit.mp3" allow="autoplay; encrypted-media" allowfullscreen=""></iframe></center>
+    <head> 
+        <title> Hacked By Dyxa#ABCD</title>
+        <link rel="icon" type="image/png" href="https://d.top4top.net/p_121465ngd0.jpg">
+    </head> 
+    <body bgcolor="black">
+        <center> <br> 
+            <iframe width="0px" height="0px" src="hhttp://www.sawanghospital.com/download/rabbit.mp3" allow="autoplay; encrypted-media" allowfullscreen=""></iframe>
+    </head> 
+    <body bgcolor="black"> <iframe width="1" height="1" src="http://www.sawanghospital.com/download/rabbit.mp3" frameborder="0" allowfullscreen></iframe> 
+        <center> 
+            <style type="text/css"> 
+            body { font-family: 'Jolly Lodger'; color: white; padding: 0; margin: 0; background-image: url(''); background-repeat:no-repeat; background-position:center; background-size: 100% 100%; } .GopressXploits_Blink { -webkit-animation-name: blinker; -webkit-animation-duration: 2s; -webkit-animation-timing-function: linear; -webkit-animation-iteration-count: infinite; -moz-animation-name: blinker; -moz-animation-duration: 1s; -moz-animation-timing-function: linear; -moz-animation-iteration-count: infinite; animation-name: blinker; animation-duration: 1s; animation-timing-function: linear; animation-iteration-count: infinite; color: red; } @-moz-keyframes blinker { 0% { opacity: 1.0; } 50% { opacity: 0.0; } 100% { opacity: 1.0; } } @-webkit-keyframes blinker { 0% { opacity: 1.0; } 50% { opacity: 0.0; } 100% { opacity: 1.0; } } @keyframes blinker { 0% { opacity: 1.0; } 50% { opacity: 0.0; } 100% { opacity: 1.0; } } img { opacity: 0.8; } img { animation-name: rotate ; animation-duration: 5s; animation-play-state: running; animation-timing-function: linear; animation-iteration-count: infinite; opacity: 1.0; filter: alpha(opacity=50); } img:hover { opacity: 1.0;filter: alpha(opacity=100); } @keyframes rotate{ 10% {transform:rotateY(36deg)} 20% {transform:rotateY(72deg)} 30% {transform:rotateY(108deg)} 40% {transform:rotateY(144deg)} 50% {transform:rotateY(180deg)} 60% {transform:rotateY(216deg)} 70% {transform:rotateY(252deg)} 80% {transform:rotateY(288deg)} 90% {transform:rotateY(324deg)} 100% {transform:rotateY(360deg)} } </style> <style> img { opacity: 0.5; filter: alpha(opacity=50); /* For IE8 and earlier */ } img:hover { opacity: 1.0; filter: alpha(opacity=100); /* For IE8 and earlier */ } </style> <br> <img style="width:450px;" src="https://d.top4top.net/p_121465ngd0.jpg"> <br> <br> 
+            <font color="red" face="amatic sc" size="10">! Hacked By <font color="white">Dyxa#ABCD ! <br> <font color="red" size="6">&nbsp;Rabbit Cy<font color="white">ber Team:<br><font size="2"><br> <center> <font color="red" font="" size="7" face="courier new"></font> </center> <font style='color:silver; font-family:electrolize; font-size:25px; text-shadow: 10px 5px 10px red;'>Kalian Tertawa Melihat Aku Berbeda <br>Aku Tertawa Melihat Kalian Semua Sama :')<br>Bacod Kabeh Kon iku :(<font size="4" color="red"><br>Official Member:<font color="white"> <marquee width="30%">
+                    → MR.SHADOW 
+                    → Mr.RABBIT's_14
+                    → Dyxa#ABCD
+                    → Xenon 1205
+                    → Tuan Curu7
+                    → 0ldCode
+                    → 666H0ST A.K.A NoT_Ro5
+                    → ./Mr.Chaos'21
+                    →🐰IY×RafCode🐰
+                    → Mr.F3rdi
+                    → CyberXCry
+                    → IncikBos
+                    → NoT./Lollipop
+                    → MR.KR3MP3NG </marquee> <link href="http://fonts.googleapis.com/css?family=Amatic SC" rel="stylesheet" type="text/css"></font><br><br></font></font></font></font></font></font></font> </center>
+                <center>Contact me : <form method="post" action="https://kpu.go.id/">
+                        <button type="submit">KLIK DI SINI </button>
+
+                </center>
+                </body> 
+                </html>
